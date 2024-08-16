57e1930312025b8080aa6f11d9d355b9a905c009
Achraf-Kallel <kallelachraf58@gmail.com>
2024-08-17 00:27:08 +0100

    prototype 2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-
-
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  protected showForm: boolean = false; //initialize  the form to false
 
+  onEmployeeAdd() {
+    this.showForm = !this.showForm;
+    console.log('Add employee button clicked');
+  }
 }
